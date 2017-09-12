134f72e798b63d0d968080ce84874de59d98775c
Torben Weibert <tw@mairlist.com>
2017-09-12 16:11:59 +0200

    Alten Debug-Code entfernt

--- ohrenbaer.php
+++ ohrenbaer.php
@@ -5,15 +5,12 @@ function sanitize($fn) {
 }
 
 $xml = simplexml_load_file('https://www.ohrenbaer.de/podcast/podcast.xml/feed=podcast.xml');
-# $xml = simplexml_load_file('feed.xml');
 
 
 foreach ($xml->channel[0]->item as $item) {
-#  print_r($item);
   $desc = trim($item->description);
   $url = trim($item->link);
   
-#  if (! preg_match("/^Aus der .*Radiogeschichte( zur ARD.Themenwoche)?(\: | \"\:?)(.*?)\"?\s?\(Folge (\d+) von (\d+)\) von (.*)\. Es liest: (.*)\./", $desc, $m)) {
   if (! preg_match("/^Aus der .*Radiogeschichte(\: (.*)| \"(.*?)\")\s?\(Folge (\d+) von (\d+)\) von (.*)\. Es liest: (.*)\./", $desc, $m)) {
     print "Fehler beim Parsen der Beschreibung für $url: $desc\n";
     continue;
@@ -25,8 +22,6 @@ foreach ($xml->channel[0]->item as $item) {
   $folgevon = $m[5];
   $autor = $m[6];
   $sprecher = $m[7];
-  
-#  print "$geschichte, $titel, Folge $folge von $folgevon, von $autor\n";
   
   $dir = sanitize($geschichte);
   
